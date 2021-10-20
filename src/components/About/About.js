913e7c2b1ba41fb60e58b0be7bd5f72ab643c0e2
Imran <imranimon1@gmail.com>
2021-10-20 14:20:47 +0200

    about page edited

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,14 +8,14 @@ const About = () => {
                 <Row className="w-100">
                     <h1 className='text-secondary'>Welcome to the NFam Diagnostics And Hospital</h1>
                     <p className="mt-2">NFam Diagnostics And Hospital Limited is a tertiary care hospital and the leading contributor of private healthcare services in Bangladesh. This has been achieved only through consistent commitment to improve the lives of people through utmost service excellence since its inception on 16th December, 2006. NFam Diagnostics And Hospital Limited is one of the ventures of NFam Group which is the top business group of the country.</p>
-                    <p className="mt-2">The reputation of Square Hospital is the result of quality clinical outcome and comprehensive care, made achievable through world class integrated healthcare facilities by highly trained professionals. Thus, Square Hospitals strives to meet patients’ standards through quality healthcare and making a difference in their lives.</p>
+                    <p className="mt-2">The reputation of NFam Diagnostics And Hospital is the result of quality clinical outcome and comprehensive care, made achievable through world class integrated healthcare facilities by highly trained professionals. Thus, NFam Diagnostics And Hospital strives to meet patients’ standards through quality healthcare and making a difference in their lives.</p>
 
                 </Row>
                 <Row className="mt-5 w-100">
                     <h1 className='text-secondary'>We Care For Your Health</h1>
                     <ListGroup className="mt-2">
                         <ListGroup.Item>
-                            <span className="fw-bold fst-italic">Best Child Specialist:</span> Department of Pediatrics II (Pediatric Nephrology, Gastroenterology, and Endocrinology) at University Hospital Essen provides care for children and adolescents suffering from diseases affecting their kidney, urinary system, gastrointestinal tract, pancreas, biliary tract, liver, and endocrinal glands as well as nutritional disorders.
+                            <span className="fw-bold fst-italic">Best Child Specialist:</span> Department of Pediatrics II (Pediatric Nephrology, Gastroenterology, and Endocrinology) at NFam Diagnostics And Hospital provides care for children and adolescents suffering from diseases affecting their kidney, urinary system, gastrointestinal tract, pancreas, biliary tract, liver, and endocrinal glands as well as nutritional disorders.
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <span className="fw-bold fst-italic">Best Laboratories:</span> As our understanding of clinical chemistry advances, so too does our ability to test for the markers of disease and bodily imbalance. Medical testing laboratories (also known as clinical laboratories) are laboratories which have all the necessary equipment and staff to perform clinical pathology testing on patient specimens.
