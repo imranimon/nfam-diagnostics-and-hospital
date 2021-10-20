bf7aa65649c395e8ce4768e1a1346e7a9e04eb47
Imran <imranimon1@gmail.com>
2021-10-20 14:17:16 +0200

    not-found page edited

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div className='Container d-flex justify-content-center'>
+        <div className='Container d-flex justify-content-center bg-404'>
             <div>
-                <div>
-                    <img className='img-fluid' src="https://i.ibb.co/wzM3VQ7/404-fehler-page-seiten-titel.jpg" alt="" />
-                </div>
-                <div className="d-flex justify-content-center">
+                <div className='btn-position'>
                     <Link to='/home'>
                         <button className='btn btn-outline-success m-2'>Back to Home</button>
                     </Link>
                 </div>
+                <div>
+                    <img className='img-fluid' src="https://i.ibb.co/wzM3VQ7/404-fehler-page-seiten-titel.jpg" alt="" />
+                </div>
+
             </div>
         </div>
     );
