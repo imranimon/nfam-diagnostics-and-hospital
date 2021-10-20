75c83ea55b08791b370842e61de4821254eaa597
Imran <imranimon1@gmail.com>
2021-10-20 14:29:45 +0200

    not found page changed

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     return (
         <div className='Container d-flex justify-content-center bg-404'>
             <div>
-                <div className='btn-position'>
+                <div className='d-flex justify-content-center'>
                     <Link to='/home'>
                         <button className='btn btn-outline-success m-2'>Back to Home</button>
                     </Link>
